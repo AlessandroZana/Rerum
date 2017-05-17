c8d58466778da983a2a391b703ec491d7c02baf5
WIN10-DESKPEX\pecch <pecchio10@gmail.com>
2017-05-17 22:32:03 +0200

    Creazione livello Svilupparty

---
+++ RepoRerum/Rerum 4.15/Source/Rerum/DoorComponent.h
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "PlatformMoviment.h"
+#include "Components/ActorComponent.h"
+#include "PlatformMoviment.h"
+#include "DoorComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOpenDoor);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RERUM_API UDoorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorComponent();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	void DoorMoviment(AActor * value);
+
+	UPROPERTY(EditAnywhere)
+	AActor* Telaio;//attore contenente il telaio della porta (attualmente non più usato)
+
+	UPROPERTY(EditAnywhere)
+	AActor* Botton1;//attore che contiene il primo pulsante da premere per apire la porta
+
+	UPROPERTY(EditAnywhere)
+	AActor* Botton2;//attore che contiene il secondo pulsante da premere per apire la porta
+
+	UPROPERTY(EditAnywhere)
+	AActor* Botton3;//attore che contiene il terzo pulsante da premere per apire la porta
+
+	UPROPERTY(EditAnywhere)
+	bool IsSpecialDoor;//booleano che gestisce se stiamo lavorando su una porta normale oppure ad apertura doppia
+
+	UPROPERTY(EditAnywhere)
+	AActor* LeftDoor;//nel caso di special door contiene l'anta di sinistra
+
+	UPROPERTY(EditAnywhere)
+	AActor* RightDoor;//nel caso di special door contiene l'anta di destra
+	
+	UPROPERTY(EditAnywhere)
+	float destroyDoor = 4.f;//Tempo di delay per distruzione porta
+	
+	UPROPERTY(EditAnywhere)
+	float DelayPlatform = 20;//tempo dopo il quale le piattaforme non sono più premute
+
+	UPROPERTY(BluePrintAssignable)
+	FOpenDoor Open;//Evento che apre la porta
+
+	UFUNCTION(BluePrintCallable, Category = Default)
+	void OpenWithLeva();
+
+private:
+	int Count = 0;//contiene il numero di pulsanti da premere che varia da 1 a 3
+	int Pressed = 0;//contiene il numero di pulsanti premuti rispettando l'ordine stabilito
+
+	float delay2 = 0.8f;//Tempo di delay per distruzione porta
+	float time ;
+	float time2;
+
+	bool CanBeDestroy = false;//Variabile che stabilisce l'apertura della porta e di conseguenza la distruzione delle ante
+
+	void DelayDestroy();//Funzione che distrugge le ante
+
+	AActor* Number1 = nullptr;
+	AActor* Number2 = nullptr;
+	AActor* Number3 = nullptr;
+};
---
+++ RepoRerum/Rerum 4.15/Source/Rerum/RerumCharacter.h
@@ -0,0 +1,105 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+#include "PaperCharacter.h"
+#include "RerumCharacter.generated.h"
+
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE(FColorEvent);
+
+// This class is the default character for Rerum, and it is responsible for all
+// physical interaction between the player and the world.
+//
+//   The capsule component (inherited from ACharacter) handles collision with the world
+//   The CharacterMovementComponent (inherited from ACharacter) handles movement of the collision capsule
+//   The Sprite component (inherited from APaperCharacter) handles the visuals
+class UTextRenderComponent;
+class UOrbis;
+
+UCLASS(config=Game)
+class ARerumCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	/////////////////////////////////////////////////////////////////////////////////////	
+
+	/** Side view camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* SideViewCameraComponent;
+
+	/** Camera boom positioning the camera beside the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	UTextRenderComponent* TextComponent;
+	
+	FVector getCapsulePosition;
+	FVector setcapsulePosition;
+	float capsuleUp;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+protected:
+
+	//Animazioni orbis leggero
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* walkLight;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animations")
+	class UPaperFlipbook* idleLight;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jumpLightUp;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jumpLightDown;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jetpackLight;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jetpackLightDown;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* airDashLight;
+
+	//Animazioni orbis pesante
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* walkHeavy;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* idleHeavy;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jumpHeavyUp;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jumpHeavyDown;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* jetpackHeavy;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* dashHeavy;
+	UPROPERTY(EditDefaultsOnly, Category = "Animations")
+	class UPaperFlipbook* smashHeavy;
+
+	/** Called to choose the correct animation to play based on the character's movement state */
+	void UpdateAnimation();
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	void UpdateCharacter();
+
+	/** Handle touch inputs. */
+	void TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+	/** Handle touch stop event. */
+	void TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	// End of APawn interface
+	
+	//Puntatore alla classe Orbis
+	UOrbis* orbis = nullptr;
+public:
+	ARerumCharacter();
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void CastToOrbis(UOrbis* orbisComponent);
+	/** Returns SideViewCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+};
+
+						
